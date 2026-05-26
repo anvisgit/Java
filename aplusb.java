781466a93dceda6a884ac91068ffcfd95961c98a
Anvi Wadhwa <anvi.wadhwa2024@vitstudent.ac.in>
2026-05-27 00:16:00 +0530

    easy

---
+++ aplusb.java
@@ -0,0 +1,9 @@
+import java.util.*; 
+public class aplusb{
+  public static void main(string[] args){
+    Scanner sc=ne Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    System.out.println(a+b);
+  }
+}
