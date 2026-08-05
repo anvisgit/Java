51c9b16e624669331d6e7d53e2279377526024bf
Anvi Wadhwa <anvi.wadhwa2024@vitstudent.ac.in>
2026-08-05 20:38:39 +0530

    prime number lol

---
+++ prime.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter number");
+    int n=sc.nextInt();
+    boolean prime=false;
+    for(int i=2;i<=n/2;i++){
+      if (n%i!=0){
+        prime=true;
+        break;
+      }
+    }
+    if(prime){
+      System.out.println("Prime");
+    }else{
+      System.out.println("Not Prime");
+    }
+  }
+      
