b8c71c747b341011dec8333d383228f287dc8df8
Anvi Wadhwa <anvi.wadhwa2024@vitstudent.ac.in>
2026-08-14 03:42:18 +0530

    Create pno.java

---
+++ pno.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class prime{
+  public static vid main(String[] args){
+    Scanner sc= new Scanner(System.in);
+    for(int i=2;i<=n;i++){
+      boolean prime=true;
+      for(int j=2j<=i;j++){
+        if (i%j==0)
+          prime= false;
+          break;
+      }
+    }
+    if(prime){
+      System.out.println(i+" ");
+    }
+  }
+}
+
